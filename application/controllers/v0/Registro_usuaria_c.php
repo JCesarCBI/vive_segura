1ffe238dcb9eb26e36db5152e79c8f34145b2749
Cesar Padilla <JCesarCBI@Mac-mini-de-ldcbi.local>
2016-02-26 12:41:08 -0600

    Configuracion API RestController

---
+++ application/controllers/v0/Registro_usuaria_c.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	require(APPPATH.'/libraries/REST_Controller.php');
+	
+	class Registro_usuaria_c extends REST_Controller {
+			
+		function __construct() {
+			parent::__construct();
+		}
+		
+		public function registro_get()
+		{
+			
+		}
+	}
+	
+?>
